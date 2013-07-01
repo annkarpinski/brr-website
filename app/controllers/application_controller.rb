6aaf7efe51936fcfda6a2d6d12d52824bade636f
Ann Karpinski <annkarpinski@gmail.com>
2013-07-01 17:56:53 -0600

    Fixed navbar routing issues - except for blog.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :find_member
+  
+  def find_member
+    if session[:member_id]
+      @member = Member.find(session[:member_id])
+    end
+  end
+
 end
